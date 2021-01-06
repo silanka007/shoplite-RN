54819366d74f4597397ef5369e48b3c449e70bfc
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-01-06 18:33:27 +0100

    creating carContainer and rendering background image in rn

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world from Paul Silanka!</Text>
+      <View style={styles.carContainer}>
+        <ImageBackground source={require("./assets/images/ModelS.jpeg")} style={styles.backgroundImg} />
+        <View style={styles.heading}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>starting at $69,000</Text>
+        </View>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +26,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  heading: {
+    marginTop: "30%",
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "500"
+  },
+  subTitle: {
+    fontSize: 16
+  },
+  backgroundImg: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    resizeMode: "cover"
+  }
 });
